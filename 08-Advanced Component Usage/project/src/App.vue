0c91e3e0dfa335a20e0ef6caf2e17ac5fc4006d4
wliang <befree2008@gmail.com>
2017-07-29 14:04:14 +0800

    adv component

---
+++ 08-Advanced Component Usage/project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12">
+        <app-qoute qoute="A wonderful Qoute from Parent!">
+          <h2 slot='qouteTitle'>{{ qouteTitle }}</h2>
+          <div slot='qouteContent'>
+            <p>qoute Paragraph one</p>
+            <p>qoute Paragraph two</p>
+          </div>
+          <p slot='subTitle'>Vue is really amazing</p>
+        </app-qoute>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Qoute from './component/Qoute.vue';
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      qouteTitle:"The Qoutes"
+    };
+  },
+  components : {
+    'app-qoute': Qoute
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 08-Advanced Component Usage/project/src/component/Qoute.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <div class="">
+    <div class="title">
+      <slot name="qouteTitle"></slot>
+      <span><slot name="subTitle">this is subTitle</slot></span>
+    </div>
+    <hr>
+    <div class="qouteContent">
+      <p>A Wonderful Qoute!</p>
+      <p>{{ qoute }}</p>
+      <slot name="qouteContent"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['qoute']
+}
+</script>
+
+<style lang="css" scoped="">
+  div.title {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px  black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+
+  .qouteContent {
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+
+  span {
+    color : #ccc;
+  }
+
+  h2 {
+    color: red;
+  }
+</style>
