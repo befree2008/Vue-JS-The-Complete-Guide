5feb2bce49d156490599b4bb52c91adc7b219646
wliang <befree2008@gmail.com>
2017-07-30 16:25:47 +0800

    form

--- 10-Handling User Input with Forms/course/src/App.vue
+++ 10-Handling User Input with Forms/course/src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="app" class='container'>
-    <form>
+    <form v-if="!isSubmited">
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <h1>File a complaint</h1>
           <hr>
           <div class="form-group">
             <label for="email">Email address</label>
-            <input type="email" class="form-control" id="email" placeholder="Email">
+            <input type="email"
+                   class="form-control"
+                   id="email"
+                   placeholder="Email"
+                   v-model.lazy='userData.email'>
           </div>
           <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" placeholder="Password">
+            <input type="password" class="form-control" id="password"
+                   placeholder="Password"
+                   v-model='userData.passwd'>
           </div>
 
           <div class="form-group">
             <label for="age">Age</label>
-            <input type="text" class="form-control" id="age" placeholder="Age">
+            <input type="text" class="form-control" id="age" placeholder="Age"
+                   v-model.number='userData.age'>
           </div>
         </div>
       </div>
@@ -25,7 +32,8 @@
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <label for="message">Message</label>
           <br>
-          <textarea class="form-control" id="message" rows=5>
+          <textarea class="form-control" id="message" rows=5
+                    v-model="message">
           </textarea>
 
         </div>
@@ -34,11 +42,13 @@
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="sendmail">
-              <input type="checkbox" id='sendmail' value='SendMail'>
+              <input type="checkbox" id='sendmail' value='SendMail'
+                     v-model='sendMail'>
               SendMail
             </label>
             <label for="sendInformail">
-              <input type="checkbox" id='sendInformail' value='SendInforMail'>
+              <input type="checkbox" id='sendInformail' value='SendInforMail'
+              v-model='sendMail'>
               SendInformail
             </label>
           </div>
@@ -49,57 +59,70 @@
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="male">
-              <input type="radio" id='male' value='Male'>
+              <input type="radio" id='male' value='Male' name='gender'
+                     v-model='gender'>
               Male
             </label>
             <label for="female">
-              <input type="radio" id='female' value='Female'>
+              <input type="radio" id='female' value='Female' name='gender'
+                     v-model='gender'>
               Female
             </label>
           </div>
         </div>
       </div>
-
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <app-switched v-model='mySwitch'></app-switched>
+        </div>
+      </div>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="priority">Priority</label>
-            <select class="form-control" id='priority'>
-              <option value=""></option>
+            <select class="form-control" id='priority'
+                    v-model='selPriority'>
+              <option v-for="priority in priorities"
+                      :selected="priority == 'Medium'" >
+                      {{ priority }}
+              </option>
             </select>
           </div>
         </div>
 
       </div>
-
+<hr>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <hr>
-          <button type="submit" class="btn btn-primary">Submit</button>
+
+          <button type="submit" class="btn btn-primary pull-right"
+                   @click.prevent="submitted">Submit</button>
         </div>
       </div>
     </form>
-
-    <div class="row">
+    <hr>
+    <div class="row" v-if="isSubmited">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <hr>
+
         <div class="panel panel-default">
           <div class="panel-heading">
             <h3 class="panel-title">Your Data</h3>
           </div>
           <div class="panel-body">
-            <p>Mail:</p>
-            <p>Password:</p>
-            <p>Age:</p>
-            <p>Message:</p>
+            <p>Mail: {{userData.email}}</p>
+            <p>Password: {{userData.passwd}}</p>
+            <p>Age: {{userData.age}}</p>
+            <p style="white-space:pre">Message:  {{message}}</p>
             <p><strong>Send Mail?</strong></p>
             <ul>
-              <li></li>
+              <li v-for='item in sendMail'>{{item}}</li>
             </ul>
-            <p>Gender:</p>
-            <p>Priority:</p>
+            <p>Gender: {{gender}}</p>
+            <p>Priority: {{selPriority}}</p>
+            <p>Switch: {{mySwitch}}</p>
           </div>
         </div>
+        <button class="btn btn-success pull-right" @click="isSubmited = !isSubmited">Reset</button>
       </div>
     </div>
 
@@ -107,8 +130,37 @@
 </template>
 
 <script>
+import Switched from './component/switched.vue'
+
 export default {
   name: 'app',
+
+  data : function(){
+    return {
+      userData : {
+        email: '',
+        passwd: '',
+        age: 36
+      },
+      message : 'Input your complaint',
+      sendMail : [],
+      gender : 'Male',
+      priorities : ['High', 'Medium', 'Low'],
+      selPriority: "High",
+      mySwitch : true,
+      isSubmited : false
+    };
+  },
+
+  components : {
+    appSwitched : Switched,
+  },
+
+  methods :{
+    submitted(){
+      this.isSubmited = true;
+    }
+  }
 
 }
 </script>
