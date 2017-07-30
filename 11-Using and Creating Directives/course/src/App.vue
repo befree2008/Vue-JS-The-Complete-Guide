e282065d70630eae07afd9f2203192271a127b58
wliang <befree2008@gmail.com>
2017-07-30 23:30:27 +0800

    custom directive

---
+++ 11-Using and Creating Directives/course/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1>Directive</h1>
+          <p v-text="'some text'"></p>
+          <p v-html="'<strong>Some Strong Text</strong>'"></p>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <h1>Custom Directive</h1>
+          <p v-highlight="'lightgrey'">custom directive</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+}
+</script>
+
+<style>
+
+</style>
